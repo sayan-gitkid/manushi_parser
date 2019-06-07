2361a87a2c2c4c858b325a7d39ab3c46126c55be
sayan <sayan.shakya@gmail.com>
2019-06-07 08:21:38 +0545

    updates

--- execution/run.py
+++ execution/run.py
@@ -1,16 +1,29 @@
 from library.dbConnector import DbConn
+import datetime
 
 
-def export_table():
-    pass
+def today_date():
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def migrate_units(old_db, new_db):
+    old_units = old_db.exec_query("select * from product_unit")
+    new_units = new_db.exec_query("select * from product_unit")
+    print(today_date())
 
 
 def main():
     mysql = DbConn(db='db_manushi_old',
                    user='root',
                    password='password')
-    tables = mysql.exec_query("show tables")
-    mysql.export_tables(mysql)
+
+    to_db = DbConn(db='manushi',
+                   user='root',
+                   password='password')
+    # tables = mysql.exec_query("show tables")
+    # mysql.export_tables(mysql)
+    migrate_units(mysql, to_db)
 
     asdf = 10
 
